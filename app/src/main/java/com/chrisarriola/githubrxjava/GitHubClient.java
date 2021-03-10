da521bd2f68beff7f1e3f7c53d5d2c051ffe52e7
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-10 20:40:12 +0200

    refactored

--- app/src/main/java/com/chrisarriola/githubrxjava/GitHubClient.java
+++ app/src/main/java/com/chrisarriola/githubrxjava/GitHubClient.java
@@ -1,6 +1,6 @@
 package com.chrisarriola.githubrxjava;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
